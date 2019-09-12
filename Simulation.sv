73ed53447fba4c75bf7785d3410f6e930a66da0a
Arthur <ahac@cin.ufpe.br>
2019-09-12 18:10:38 -0300

    Corrigido - Pt1

--- Simulation.sv
+++ Simulation.sv
@@ -4,8 +4,10 @@ module Simulation;
     localparam PERIODO = CLK/2;
     logic clock;
     logic reset;
+    logic WR_MEM;
+    logic WR_INST;
 
-    UP processing(.CLK(clock), .RST(reset));
+    UP processing(.CLK(clock), .RST(reset),.WR_MEM_INSTR(WR_MEM), .WRITE_INSTRUCTION(WR_INST) );
     
     initial begin
         clock = 1'b1;
