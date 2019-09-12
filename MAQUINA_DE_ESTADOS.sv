f3e54e5d4140c70fe9d2195bfd0ca03956c2cf25
Arthur <ahac@cin.ufpe.br>
2019-09-12 18:07:45 -0300

    Corrigido - Pt1

--- MAQUINA_DE_ESTADOS.sv
+++ MAQUINA_DE_ESTADOS.sv
@@ -10,46 +10,46 @@ module MAQUINA_DE_ESTADOS  (input CLK,
 
     always_ff @(posedge CLK, posedge RST) begin
         if(RST) estado  <= reset;
-        else estado <= prox_estado
+        else estado     <= prox_estado;
     end
     
     
     always_comb begin
         case(estado)
             reset:begin
-                WRITE_INSTRUCTION   = 0;
-                WR_MEM_INSTR        = 1;
-                reset_wire          = 1;
+                WRITE_INSTRUCTION   = 1'b0;
+                WR_MEM_INSTR        = 1'b1;
+                reset_wire          = 1'b1;
                 operacao            = 3'b000;
-                WRITE_PC            = 0;
+                WRITE_PC            = 1'b0;
                 prox_estado         = espera;
             end
 
             soma:begin
-                WRITE_INSTRUCTION   = 0;
-                WR_MEM_INSTR        = 0;
-                reset_wire          = 0;
+                WRITE_INSTRUCTION   = 1'b0;
+                WR_MEM_INSTR        = 1'b0;
+                reset_wire          = 1'b0;
                 operacao            = 3'b001;
                 WRITE_PC            = 1'b1;
                 prox_estado         = load_reg;
             end
             
             espera:begin
-                WRITE_INSTRUCTION   = 0;
+                WRITE_INSTRUCTION   = 1'b0;
                 WR_MEM_INSTR        = 1'b1;
-                reset_wire          = 0;
-                operacao            = 0;
-                WRITE_PC            = 0;
+                reset_wire          = 1'b0;
+                operacao            = 1'b0;
+                WRITE_PC            = 1'b0;
                 prox_estado         = soma;
             end
            
            load_reg:begin
-                WRITE_INSTRUCTION   = 0;
+                WRITE_INSTRUCTION   = 1'b0;
                 WR_MEM_INSTR        = 1'b1;
                 prox_estado         = espera;
                 operacao            = 3'b000;
-                reset_wire          = 0;
-                WRITE_PC            = 0;
+                reset_wire          = 1'b0;
+                WRITE_PC            = 1'b0;
             end
 
         endcase
